3522699901c4f5217b0edfcc07c1d83b1e4614fd
dkonovalova <dkonovalova@gmail.com>
2021-01-20 11:24:34 +0200

    add costructors and desrtructor

--- node.h
+++ node.h
@@ -25,29 +25,39 @@ enum class LogicalOperation
 class Node
 {
 public:
+	Node() = default;
 	virtual bool Evaluate(const Date& date, const string& event) = 0;
+	virtual ~Node() = default;
 };
 
 class DateComparisonNode : public Node 
 {
 public:
+	DateComparisonNode() = default;
+	DateComparisonNode(const Comparison& cmp, const Date& date);
 	bool Evaluate(const Date& date, const string& event) override;
 };
 
 class EventComparisonNode : public Node
 {
 public:
+	EventComparisonNode() = default;
+	EventComparisonNode(const Comparison& cmp, const string& event);
 	bool Evaluate(const Date& date, const string& event) override;
 };
 
 class LogicalOperationNode : public Node
 {
 public:
+
+	LogicalOperationNode() = default;
+	LogicalOperationNode(const LogicalOperation& logical_operation, const shared_ptr<Node>& left, const shared_ptr<Node>& right);
 	bool Evaluate(const Date& date, const string& event) override;
 };
 
 class EmptyNode : public Node
 {
 public:
+	EmptyNode() = default;
 	bool Evaluate(const Date& date, const string& event) override;
 };
