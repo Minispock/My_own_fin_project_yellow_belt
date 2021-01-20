458044b2647f619c4e7d49c77d7a46dc05bdd8c7
dkonovalova <dkonovalova@gmail.com>
2021-01-20 13:05:23 +0200

    delete default constructors

--- node.h
+++ node.h
@@ -33,7 +33,6 @@ public:
 class DateComparisonNode : public Node 
 {
 public:
-	DateComparisonNode() = default;
 	DateComparisonNode(const Comparison& cmp, const Date& date);
 	bool Evaluate(const Date& date, const string& event) override;
 private:
@@ -44,7 +43,6 @@ private:
 class EventComparisonNode : public Node
 {
 public:
-	EventComparisonNode() = default;
 	EventComparisonNode(const Comparison& cmp, const string& event);
 	bool Evaluate(const Date& date, const string& event) override;
 private:
@@ -55,7 +53,6 @@ private:
 class LogicalOperationNode : public Node
 {
 public:
-	LogicalOperationNode() = default;
 	LogicalOperationNode(const LogicalOperation& logical_operation, const shared_ptr<Node>& left, const shared_ptr<Node>& right);
 	bool Evaluate(const Date& date, const string& event) override;
 private:
@@ -67,6 +64,5 @@ private:
 class EmptyNode : public Node
 {
 public:
-	EmptyNode() = default;
 	bool Evaluate(const Date& date, const string& event) override;
 };
